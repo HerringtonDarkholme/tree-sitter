8e2ca00673eadc57acf75a21032b78d905fbf806
HerringtonDarkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2026-02-06 20:02:48 -0500

    delete C

--- lib/src/lib.c
+++ lib/src/lib.c
@@ -1,13 +1,9 @@
-#include "./alloc.c"
-#include "./get_changed_ranges.c"
-#include "./language.c"
-#include "./lexer.c"
-#include "./node.c"
-#include "./parser.c"
-#include "./point.c"
+// Tree-sitter C library entry point (amalgamated build)
+// Most modules have been migrated to Rust (see lib/src_rust/).
+// Only query.c and wasm_store.c remain in C.
+//
+// For Rust builds: see remaining_lib.c
+// For pure C builds: use the individual .c files or link against the Rust library
+
 #include "./query.c"
-#include "./stack.c"
-#include "./subtree.c"
-#include "./tree_cursor.c"
-#include "./tree.c"
 #include "./wasm_store.c"
