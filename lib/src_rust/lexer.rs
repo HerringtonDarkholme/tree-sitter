fd58beaeb82581d6179018baa3f0763d89ce7ecb
HerringtonDarkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2026-06-24 18:45:53 -0400

    Use reference in lexer eof

--- lib/src_rust/lexer.rs
+++ lib/src_rust/lexer.rs
@@ -123,8 +123,8 @@ fn ts_lexer__invalidate_column_data(self_: &mut Lexer) {
 
 /// Check if the lexer has reached EOF.
 unsafe extern "C" fn ts_lexer__eof(_self: *const TSLexer) -> bool {
-    let self_ = _self as *const Lexer;
-    (*self_).current_included_range_index == (*self_).included_range_count
+    let self_ = &*(_self as *const Lexer);
+    self_.current_included_range_index == self_.included_range_count
 }
 
 /// Clear the currently stored chunk of source code.
