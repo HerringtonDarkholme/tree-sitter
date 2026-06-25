ecea8a561e0d9412ea1e9c8d3af9d3d9372d1557
HerringtonDarkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2026-06-25 11:41:11 -0400

    Annotate parser range transmute

--- lib/src_rust/parser.rs
+++ lib/src_rust/parser.rs
@@ -2654,7 +2654,8 @@ pub unsafe extern "C" fn ts_parser_new() -> *mut TSParser {
     parser.operation_count = 0;
     parser.old_tree = NULL_SUBTREE;
     let new_array: Array<TSRange> = array_new();
-    parser.included_range_differences = core::mem::transmute(new_array);
+    parser.included_range_differences =
+        core::mem::transmute::<Array<TSRange>, TSRangeArray>(new_array);
     parser.included_range_difference_index = 0;
     ts_parser__set_cached_token(parser, 0, NULL_SUBTREE, NULL_SUBTREE);
     self_
