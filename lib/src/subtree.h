b92b2bc08fb667d65cf4fd047060ed90bcb82a35
HerringtonDarkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2026-06-25 12:36:49 -0400

    Trim unused subtree header helpers

--- lib/src/subtree.h
+++ lib/src/subtree.h
@@ -7,10 +7,8 @@ extern "C" {
 
 #include <limits.h>
 #include <stdbool.h>
-#include <stdio.h>
 #include "./length.h"
 #include "./array.h"
-#include "./error_costs.h"
 #include "./host.h"
 #include "tree_sitter/api.h"
 #include "./parser.h"
@@ -173,167 +171,14 @@ typedef struct {
   MutableSubtreeArray tree_stack;
 } SubtreePool;
 
-#define SUBTREE_GET(self, name) ((self).data.is_inline ? (self).data.name : (self).ptr->name)
-
-static inline TSSymbol ts_subtree_symbol(Subtree self) { return SUBTREE_GET(self, symbol); }
-static inline bool ts_subtree_visible(Subtree self) { return SUBTREE_GET(self, visible); }
-static inline bool ts_subtree_named(Subtree self) { return SUBTREE_GET(self, named); }
-static inline bool ts_subtree_extra(Subtree self) { return SUBTREE_GET(self, extra); }
-static inline bool ts_subtree_has_changes(Subtree self) { return SUBTREE_GET(self, has_changes); }
-static inline bool ts_subtree_missing(Subtree self) { return SUBTREE_GET(self, is_missing); }
-static inline bool ts_subtree_is_keyword(Subtree self) { return SUBTREE_GET(self, is_keyword); }
-static inline TSStateId ts_subtree_parse_state(Subtree self) { return SUBTREE_GET(self, parse_state); }
-static inline uint32_t ts_subtree_lookahead_bytes(Subtree self) { return SUBTREE_GET(self, lookahead_bytes); }
-
-#undef SUBTREE_GET
-
-// Get the size needed to store a heap-allocated subtree with the given
-// number of children.
-static inline size_t ts_subtree_alloc_size(uint32_t child_count) {
-  return child_count * sizeof(Subtree) + sizeof(SubtreeHeapData);
-}
-
-// Get a subtree's children, which are allocated immediately before the
-// tree's own heap data.
-#define ts_subtree_children(self) \
-  ((self).data.is_inline ? NULL : (Subtree *)((self).ptr) - (self).ptr->child_count)
-
-static inline void ts_subtree_set_extra(MutableSubtree *self, bool is_extra) {
-  if (self->data.is_inline) {
-    self->data.extra = is_extra;
-  } else {
-    self->ptr->extra = is_extra;
-  }
-}
-
-static inline TSSymbol ts_subtree_leaf_symbol(Subtree self) {
-  if (self.data.is_inline) return self.data.symbol;
-  if (self.ptr->child_count == 0) return self.ptr->symbol;
-  return self.ptr->first_leaf.symbol;
-}
-
-static inline TSStateId ts_subtree_leaf_parse_state(Subtree self) {
-  if (self.data.is_inline) return self.data.parse_state;
-  if (self.ptr->child_count == 0) return self.ptr->parse_state;
-  return self.ptr->first_leaf.parse_state;
-}
-
-static inline Length ts_subtree_padding(Subtree self) {
-  if (self.data.is_inline) {
-    Length result = {self.data.padding_bytes, {self.data.padding_rows, self.data.padding_columns}};
-    return result;
-  } else {
-    return self.ptr->padding;
-  }
-}
-
-static inline Length ts_subtree_size(Subtree self) {
-  if (self.data.is_inline) {
-    Length result = {self.data.size_bytes, {0, self.data.size_bytes}};
-    return result;
-  } else {
-    return self.ptr->size;
-  }
-}
-
-static inline Length ts_subtree_total_size(Subtree self) {
-  return length_add(ts_subtree_padding(self), ts_subtree_size(self));
-}
-
-static inline uint32_t ts_subtree_total_bytes(Subtree self) {
-  return ts_subtree_total_size(self).bytes;
-}
-
-static inline uint32_t ts_subtree_child_count(Subtree self) {
-  return self.data.is_inline ? 0 : self.ptr->child_count;
-}
-
-static inline uint32_t ts_subtree_repeat_depth(Subtree self) {
-  return self.data.is_inline ? 0 : self.ptr->repeat_depth;
+static inline TSSymbol ts_subtree_symbol(Subtree self) {
+  return self.data.is_inline ? self.data.symbol : self.ptr->symbol;
 }
 
 static inline uint32_t ts_subtree_is_repetition(Subtree self) {
   return self.data.is_inline
     ? 0
     : !self.ptr->named && !self.ptr->visible && self.ptr->child_count != 0;
-}
-
-static inline uint32_t ts_subtree_visible_descendant_count(Subtree self) {
-  return (self.data.is_inline || self.ptr->child_count == 0)
-    ? 0
-    : self.ptr->visible_descendant_count;
-}
-
-static inline uint32_t ts_subtree_visible_child_count(Subtree self) {
-  if (ts_subtree_child_count(self) > 0) {
-    return self.ptr->visible_child_count;
-  } else {
-    return 0;
-  }
-}
-
-static inline uint32_t ts_subtree_error_cost(Subtree self) {
-  if (ts_subtree_missing(self)) {
-    return ERROR_COST_PER_MISSING_TREE + ERROR_COST_PER_RECOVERY;
-  } else {
-    return self.data.is_inline ? 0 : self.ptr->error_cost;
-  }
-}
-
-static inline int32_t ts_subtree_dynamic_precedence(Subtree self) {
-  return (self.data.is_inline || self.ptr->child_count == 0) ? 0 : self.ptr->dynamic_precedence;
-}
-
-static inline uint16_t ts_subtree_production_id(Subtree self) {
-  if (ts_subtree_child_count(self) > 0) {
-    return self.ptr->production_id;
-  } else {
-    return 0;
-  }
-}
-
-static inline bool ts_subtree_fragile_left(Subtree self) {
-  return self.data.is_inline ? false : self.ptr->fragile_left;
-}
-
-static inline bool ts_subtree_fragile_right(Subtree self) {
-  return self.data.is_inline ? false : self.ptr->fragile_right;
-}
-
-static inline bool ts_subtree_has_external_tokens(Subtree self) {
-  return self.data.is_inline ? false : self.ptr->has_external_tokens;
-}
-
-static inline bool ts_subtree_has_external_scanner_state_change(Subtree self) {
-  return self.data.is_inline ? false : self.ptr->has_external_scanner_state_change;
-}
-
-static inline bool ts_subtree_depends_on_column(Subtree self) {
-  return self.data.is_inline ? false : self.ptr->depends_on_column;
-}
-
-static inline bool ts_subtree_is_fragile(Subtree self) {
-  return self.data.is_inline ? false : (self.ptr->fragile_left || self.ptr->fragile_right);
-}
-
-static inline bool ts_subtree_is_error(Subtree self) {
-  return ts_subtree_symbol(self) == ts_builtin_sym_error;
-}
-
-static inline bool ts_subtree_is_eof(Subtree self) {
-  return ts_subtree_symbol(self) == ts_builtin_sym_end;
-}
-
-static inline Subtree ts_subtree_from_mut(MutableSubtree self) {
-  Subtree result;
-  result.data = self.data;
-  return result;
-}
-
-static inline MutableSubtree ts_subtree_to_mut_unsafe(Subtree self) {
-  MutableSubtree result;
-  result.data = self.data;
-  return result;
 }
 
 #ifdef __cplusplus
