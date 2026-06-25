5b59fdba48073b4c2b124509d6d0862bd8701ef7
HerringtonDarkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2026-06-25 18:59:27 -0400

    Keep Rust-only node constructor internal

--- lib/src_rust/node.rs
+++ lib/src_rust/node.rs
@@ -650,8 +650,7 @@ unsafe fn ts_node__field_name_from_language(
 // Exported functions — constructors
 // ---------------------------------------------------------------------------
 
-#[no_mangle]
-pub unsafe extern "C" fn ts_node_new(
+pub unsafe fn ts_node_new(
     tree: *const TSTree,
     subtree: *const Subtree,
     position: Length,
