06383d2d9233d1b928b80c859c9725122e7c1360
HerringtonDarkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2026-06-25 06:33:17 -0400

    Use pointer cast in parser language setup

--- lib/src_rust/parser.rs
+++ lib/src_rust/parser.rs
@@ -2714,7 +2714,7 @@ pub unsafe extern "C" fn ts_parser_set_language(
     parser.language = ptr::null();
 
     if !language.is_null() {
-        let lang_full = language as *const TSLanguageFull;
+        let lang_full = language.cast::<TSLanguageFull>();
         if (*lang_full).abi_version > TREE_SITTER_LANGUAGE_VERSION
             || (*lang_full).abi_version < TREE_SITTER_MIN_COMPATIBLE_LANGUAGE_VERSION
         {
