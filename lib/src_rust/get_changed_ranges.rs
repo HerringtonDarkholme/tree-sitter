42db826c575ad18d9c2ebbaabd81c6bf883b0607
HerringtonDarkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2026-07-14 10:35:18 -0400

    Isolate changed range operations

--- lib/src_rust/get_changed_ranges.rs
+++ lib/src_rust/get_changed_ranges.rs
@@ -6,7 +6,6 @@ use crate::ffi::{TSInputEdit, TSLanguage, TSRange, TSSymbol};
 use super::error_costs::ERROR_STATE;
 use super::language::language_alias_at;
 use super::length::{length_add, length_min, length_zero, Length, LENGTH_MAX};
-use super::point::{point_add, point_sub, POINT_MAX};
 use super::subtree::{
     subtree_child, subtree_child_count, subtree_error_cost, subtree_external_scanner_state_eq,
     subtree_extra, subtree_has_changes, subtree_has_external_tokens, subtree_last_external_token,
@@ -14,7 +13,7 @@ use super::subtree::{
     subtree_visible, Subtree, NULL_SUBTREE, TS_BUILTIN_SYM_ERROR, TS_TREE_STATE_NONE,
 };
 use super::tree_cursor::{tree_cursor_entry_slice, TreeCursor, TreeCursorEntry};
-use super::utils::{array_new, array_push, Array};
+use super::utils::{array_new, Array};
 use super::utils::{ptr_mut, ptr_ref};
 
 // ---------------------------------------------------------------------------
@@ -63,80 +62,9 @@ struct VisibleState {
     start_byte: u32,
 }
 
-pub fn range_edit_ref(range: &mut TSRange, edit: &TSInputEdit) {
-    if range.end_byte >= edit.old_end_byte {
-        if range.end_byte != u32::MAX {
-            range.end_byte = edit.new_end_byte + (range.end_byte - edit.old_end_byte);
-            range.end_point = point_add(
-                edit.new_end_point,
-                point_sub(range.end_point, edit.old_end_point),
-            );
-            if range.end_byte < edit.new_end_byte {
-                range.end_byte = u32::MAX;
-                range.end_point = POINT_MAX;
-            }
-        }
-    } else if range.end_byte > edit.start_byte {
-        range.end_byte = edit.start_byte;
-        range.end_point = edit.start_point;
-    }
-
-    if range.start_byte >= edit.old_end_byte {
-        range.start_byte = edit.new_end_byte + (range.start_byte - edit.old_end_byte);
-        range.start_point = point_add(
-            edit.new_end_point,
-            point_sub(range.start_point, edit.old_end_point),
-        );
-        if range.start_byte < edit.new_end_byte {
-            range.start_byte = u32::MAX;
-            range.start_point = POINT_MAX;
-        }
-    } else if range.start_byte > edit.start_byte {
-        range.start_byte = edit.start_byte;
-        range.start_point = edit.start_point;
-    }
-}
-
-pub unsafe fn range_array_intersects_ref(
-    ranges: &TSRangeArray,
-    start_index: u32,
-    start_byte: u32,
-    end_byte: u32,
-) -> bool {
-    for range in ranges.as_slice().iter().skip(start_index as usize) {
-        if range.end_byte > start_byte {
-            if range.start_byte >= end_byte {
-                break;
-            }
-            return true;
-        }
-    }
-    false
-}
-
-// ---------------------------------------------------------------------------
-// Internal range and diff helpers
-// ---------------------------------------------------------------------------
-
-unsafe fn range_array_add(arr: &mut TSRangeArray, start: Length, end: Length) {
-    if let Some(last_range) = arr.as_mut_slice().last_mut() {
-        if start.bytes <= last_range.end_byte {
-            last_range.end_byte = end.bytes;
-            last_range.end_point = end.extent;
-            return;
-        }
-    }
-
-    if start.bytes < end.bytes {
-        let range = TSRange {
-            start_point: start.extent,
-            end_point: end.extent,
-            start_byte: start.bytes,
-            end_byte: end.bytes,
-        };
-        array_push(arr, range);
-    }
-}
+mod range;
+use range::range_array_add;
+pub use range::{range_array_intersects_ref, range_edit_ref};
 
 impl DiffIterator {
     /// Create a diff iterator rooted at a subtree.
